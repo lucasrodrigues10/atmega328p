b779a6de275241775446884c98c46c2e08510f22
lucas <lucasrodrigues10@outlook.com>
2017-10-22 01:55:22 -0200

    Melhorado timer operacao normal

--- timer/Debug/main.d
+++ timer/Debug/main.d
@@ -10,9 +10,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -37,7 +35,3 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
--- timer/main.c
+++ timer/main.c
@@ -1,16 +1,14 @@
 #include <avr/io.h>
-#include <avr/io.h>
 #include <avr/interrupt.h>
-#include <avr/pgmspace.h> //para usar o _NOP()
 
 #define F_CPU 16000000UL
 
 /*
-TCCR0 (Controle do timer e contador)
-TIMSK (Mascara de interrupção para timer e contador);
-TCNT0 (Registro para contagem do timer e contador).
+	www.embarcados.com.br/timers-do-atmega328-no-arduino/
+	TCCRxA/B (Controle do timer e contador)
+	TIMSK (Mascara de interrupção para timer e contador);
+	TCNTx (Registro para contagem do timer e contador).
 */
-unsigned char timecount;
 
 int main(void)
 {
@@ -19,13 +17,11 @@ int main(void)
 	DDRB |= (1<<DDD0); //seta led como output
 	PORTB |= (1<<PORTB0); //acende led
 	
+	TCCR1A = 0; //operacao normal 
+	TCCR1B = 0; //limpa o registrador
+	TCCR1B |= (1<<CS10 | 1<<CS12); //prescaler = 1024 CS12=1 CS10=1 (Clock Select)
 	
-	TCCR1A = 0; //operacao normal
-	TCCR1B = 0;
-	TCCR1B |= (1<<CS10 | 1<<CS12);
-	
-	TCNT1 = 0xC2F7;
-
+	TCNT1 = 0xC2F7; //inicia timer com estouro em (2^16-(16MHz/1024)*1Hz) = 49911 = 0xC2F7
 	TIMSK1 |= (1<<TOIE1);
 	
 	sei();
@@ -36,12 +32,7 @@ int main(void)
 	}
 }
 ISR(TIMER1_OVF_vect){
-	TCNT1 = 0xC2F7;
-	++timecount;
-	
-		//PORTB &= ~(1<<PORTB0); //acende led
-
-			PORTB ^= 0b00000001;//inverte led
-
+	TCNT1 = 0xC2F7; //reseta contador	
+	PORTB ^= (1<<PORTB0);//inverte led
 }
 
